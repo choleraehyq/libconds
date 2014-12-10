e0d705727aa14e3896d7367c71df2cf49d98138d
Cholerae <choleraehyq@gmail.com>
2014-12-10 15:36:44 +0800

    Threadpool completed

---
+++ src/threadPool/demo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "threadPool.h"
+
+int main() {
+	const int N = 4;
+	my_stl::threadPool pool(N);
+	std::vector< std::future<int> > result;
+	for (int i = 0; i < N; i++) {
+		result.emplace_back(
+			pool.enqueue(
+				[](int i) {
+					std::cout << "Hello " << i << std::endl;
+					return i*i;
+				}, i)
+			);
+	}
+	for (std::future<int> &ret : result) {
+		std::cout << ret.get() << std::endl;
+	}
+	system("pause");
+	return 0;
+}
---
+++ src/threadPool/threadPool.h
@@ -0,0 +1,79 @@
+#ifndef THREAD_POOL_H
+#define THREAD_POOL_H
+
+#include <thread>
+#include <future>
+#include <mutex>
+#include <functional>
+#include <condition_variable>
+#include <vector>
+#include <queue>
+#include <memory>
+
+namespace my_stl {
+	class threadPool {
+	public:
+		threadPool(const int& size);
+		template<typename F, typename... Args>
+		auto enqueue(F &&f, Args&&... args)
+			->std::future<typename std::result_of<F(Args...)>::type>;
+		~threadPool();
+	private:
+		bool isStop;
+		std::vector< std::thread > workers;
+		std::queue< std::function<void()> > tasks;
+		std::condition_variable cv;
+		std::mutex queue_mutex;
+	};
+	threadPool::threadPool(const int &size): isStop(false) {
+		for (int i = 0; i < size; i++) {
+			workers.emplace_back(
+				[this] {
+					for (;;) {
+						std::function<void()> task;
+						{
+							std::unique_lock<std::mutex> lock(this->queue_mutex);
+							cv.wait(lock, 
+								[this] { return this->isStop || !this->tasks.empty();} );
+							if (this->isStop && this->tasks.empty())
+								return;
+							task = std::move(this->tasks.front());
+							this->tasks.pop();
+						}
+						task();
+					}
+				}
+			);
+		}
+	}
+	template<typename F, typename... Args>
+	auto threadPool::enqueue(F&& f, Args&&... args)
+		->std::future< typename std::result_of<F(Args...)>::type >
+	{
+		using return_type = typename std::result_of<F(Args...)>::type;
+		auto task = std::make_shared< std::packaged_task< return_type()> >(
+			std::bind(f, args...));
+		//std::packaged_task< return_type() > task(std::bind(f, args...));
+		std::future<return_type> ret = task->get_future();
+		{
+			std::unique_lock<std::mutex> lock(this->queue_mutex);
+			if (isStop)
+				throw std::runtime_error("Enqueueing when threadpool is stopping!");
+			this->tasks.emplace([task]{ (*task)(); });
+		}
+		cv.notify_one();
+		return ret;
+	}
+	threadPool::~threadPool() {
+		{
+			std::unique_lock<std::mutex> lock(this->queue_mutex);
+			this->isStop = true;
+		}
+		this->cv.notify_all();
+		for (std::thread &worker : workers) {
+			worker.join();
+		}
+	}
+}
+
+#endif
